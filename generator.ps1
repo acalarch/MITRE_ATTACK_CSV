06328fd5f85a4470a6caf4490d8efe02ccbfb869
acalarch <acalarch@gmail.com>
2024-09-13 16:24:18 -0400

    Update generator.ps1

--- generator.ps1
+++ generator.ps1
@@ -1,1 +1,7 @@
- $out = foreach ($mit in $mitre.objects) {if($mit.type -eq "attack-pattern"){$name = $mit.name; $id=$mit.external_references[0].external_id; write-host $name "," $id}}
+#Step 1 read the STIX information into PowerShell
+#URL https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack.json
+$mitre = get-content -raw .\enterprise-attack.json | convertfrom-json
+ 
+#Step 2 read the first external reference from each mitre attack-pattern object
+$out = foreach ($mit in $mitre.objects) {if($mit.type -eq "attack-pattern"){$name = $mit.name; $id=$mit.external_references[0].external_id; write-host $name "," $id}}
+ 
